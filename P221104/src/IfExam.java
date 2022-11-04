bb960bbecb5c39d0b24b47560440ab5f28c473a2
Administrator <Administrator@User-2022EKRFIX>
2022-11-04 16:25:06 +0900

    오늘 수업내용

---
+++ P221104/src/IfExam.java
@@ -0,0 +1,19 @@
+//
+//public class IfExam {
+//
+//	public static void main(String[] args) {
+//		int num = 3;
+//
+//		if (num > 5) {
+//			System.out.println("A");
+//		} else if (num > 7) {
+//			System.out.println("B");
+//		} else {
+//			System.out.println("C");
+//		}
+//			System.out.println("D");
+//		}
+//
+//}
+//}
+//}
